b7e227df6a255f1a3a916b1c9386bc0087ccd974
Giridhar Kandachar <giridharkl@gmail.com>
2019-12-28 09:00:15 +0200

    added Login window

---
+++ PasswordVault/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordVault.ViewModels
+{
+    public class LoginViewModel:ObservableObject
+    {
+        private string _username;
+        private string _password;
+
+    }
+}
